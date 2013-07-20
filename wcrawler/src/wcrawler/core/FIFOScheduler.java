74bdaa386bd27b63885542f01cfd0b240509c528
Webcrawler Swd <webcrawler.swd@gmail.com>
2013-07-20 23:37:30 +0700

    Implement Scheduler

--- wcrawler/src/wcrawler/core/FIFOScheduler.java
+++ wcrawler/src/wcrawler/core/FIFOScheduler.java
@@ -1,48 +1,95 @@
-
 /**
- * Description: 
- * This is assignment project of Software Architecture and Design Course
- * The purpose of this project is to build a web crawler to get information from
- * some specific sites. In this case, crawler will get information about drug 
- * from www.drugs.com
-*/
-
+ * Description: This is assignment project of Software Architecture and Design
+ * Course The purpose of this project is to build a web crawler to get
+ * information from some specific sites. In this case, crawler will get
+ * information about drug from www.drugs.com
+ */
 /**
- * Created at: 10:32:20 AM  Jul 12, 2013
- * @author TanNhat
- * Project: wcrawler
+ * Created at: 10:32:20 AM Jul 12, 2013
+ *
+ * @author TanNhat Project: wcrawler
  */
 package wcrawler.core;
 
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import org.apache.log4j.Logger;
 import wcrawler._interface.IScheduler;
 import wcrawler.information.PageToCrawl;
 
-public class FIFOScheduler implements IScheduler{
+public class FIFOScheduler implements IScheduler {
 
+    static Logger _logger = Logger.getLogger(FIFOScheduler.class);
+    private ConcurrentLinkedQueue<PageToCrawl> _pageToCrawl = new ConcurrentLinkedQueue<PageToCrawl>();
+
+    /**
+     * Get next page to crawl from queue 
+     * @return : PageToCrawl
+     **/
     @Override
     public PageToCrawl getNextPageToCrawl() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PageToCrawl pageToCrawl = null;
+        if(_pageToCrawl.size() > 0){
+            pageToCrawl = _pageToCrawl.poll();
+        }
+        
+        return pageToCrawl;
     }
 
+    /**
+     * Add a list of pages to concurrent queue
+     * @params: List<PageToCrawl> pages
+     **/
     @Override
-    public void addPagesToCrawl(List<PageToCrawl> page) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void addPagesToCrawl(List<PageToCrawl> pages) {
+        if(pages == null){
+            throw new NullPointerException("pages");
+        }
+        
+        if(pages.isEmpty()){
+            _logger.debug("This list of pages is empty");
+            return;
+        }
+        
+        for(PageToCrawl p : pages){
+            addPage(p);
+        }
     }
 
+    /**
+     * Add a page to queue
+     * @param: page page to crawl to add to queue
+     */
+    private boolean addPage(PageToCrawl page) {
+        if (page == null) {
+            throw new NullPointerException("page");
+        }
+
+        return _pageToCrawl.add(page);
+    }
+
+    /**
+     * Clear the queue, create a new queue
+     */
     @Override
     public void clear() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        _pageToCrawl = new ConcurrentLinkedQueue<PageToCrawl>();
     }
 
+    /**
+     * Get number of page to crawl in queue
+     * @return: count 
+     */
     @Override
     public long getNumberOfPageToCrawl() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int count = 0;
+        count = _pageToCrawl.size();
+        
+        return count;
     }
 
     @Override
     public long getNumberOfCrawledPage() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
 }
