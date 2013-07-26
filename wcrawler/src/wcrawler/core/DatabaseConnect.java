519566ff451b685cd3f2b5a41c607f4f00040f8b
Đinh Xuân Bách <bachdx60343@fpt.edu.vn>
2013-07-27 00:30:50 +0700

    Change  isConnectDatabase() to isInsertDatabase

--- wcrawler/src/wcrawler/core/DatabaseConnect.java
+++ wcrawler/src/wcrawler/core/DatabaseConnect.java
@@ -6,6 +6,7 @@ package wcrawler.core;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import wcrawler.information.CrawlConfiguration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,24 +15,38 @@ import java.util.logging.Logger;
  * @author Vo Anh
  */
 public class DatabaseConnect {
+    //Use for read file config
     private CrawlConfiguration crawlConfiguration;
     private CrawlConfigurationHandler crawlConfigurationHandler ;
-    //
-    public boolean  isConnectDatabase(){
+     // Connection connect database  
+    Connection connection = null;
+    // Statement execute sql
+    Statement statement =null;
+    /*
+     * Insert data into database 
+     * 4 parameter : url, drugName, drugInfo, rawHtml
+     */
+    public boolean  isInsertDatabase(String url, String drugName, String drugInfo, String rawHtml){
+        
         crawlConfigurationHandler = new CrawlConfigurationHandler();
         crawlConfiguration = crawlConfigurationHandler.loadCrawlConfigFromXml();
-        //Create connection connect database
-        Connection connection = null;
         //Get value for variable connect database
         String driver = crawlConfiguration.getDriver();
-        String url = crawlConfiguration.getUrl();
+        String urlDatabase = crawlConfiguration.getUrl();
         String username = crawlConfiguration.getUsername();
         String password = crawlConfiguration.getPassword();
         String database = crawlConfiguration.getDatabase();
         try {
             Class.forName(driver).newInstance();
-            connection = DriverManager.getConnection(url+ database, username, password); //Get Connnect
-            return true;
+            //Get Connection
+            connection = DriverManager.getConnection(urlDatabase+ database, username, password); 
+            //sql statment for insert data
+            String sql="INSERT INTO Test(URL,Drug_Name,Drug_Infomation,Raw_Html) VALUES('"+url+"','"+drugName+"','"+drugInfo+"','"+rawHtml +"')";
+            statement = connection.createStatement();
+            //excute update sql statement
+            statement.executeUpdate(sql);
+            //return true if insert succesfully
+           return true;
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
             Logger.getLogger(CrawlConfigurationHandler.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
@@ -44,6 +59,7 @@ public class DatabaseConnect {
                 Logger.getLogger(DatabaseConnect.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+        //fail to insert
         return false;
-    }  
+    }
 }
